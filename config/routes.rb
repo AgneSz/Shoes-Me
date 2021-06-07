dbbe399460ad0b9f0c041abaa53e8d1db7afbd0b
Radha Patel <radha13.patel@gmail.com>
2021-06-07 16:27:56 +0100

    routes created

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,13 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :outfits do
+    resources :shoes, only: [:new, :create, :edit, :update]
+  end
+
+  resources :shoes, only: [:destroy] do 
+    resources :feedback, only: [:create, :update]
+  end
+
+  resources :feedback, only: [:destroy]
 end
