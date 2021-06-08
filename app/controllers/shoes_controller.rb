4a6983fdd733c7c1ed6f2d2d6874d304c268426a
Chloe Balanche <chloebalanche@gmail.com>
2021-06-08 15:31:00 +0100

    shoes controller and view

--- app/controllers/shoes_controller.rb
+++ app/controllers/shoes_controller.rb
@@ -1,5 +1,6 @@
 class ShoesController < ApplicationController
-  before_action :set_outfit, only: [:new, :create, :edit, :update, :destroy]
+  before_action :set_outfit, only: [:new, :create, :edit, :update]
+  before_action :set_shoe, only: [:edit, :update, :destroy]
 
   def new
     @shoe = Shoe.new
@@ -9,32 +10,36 @@ class ShoesController < ApplicationController
     @shoe = Shoe.new(shoe_params)
     @shoe.outfit = @outfit
     if @shoe.save
-      redirect_to outfits_path
+      flash[:notice] = "Success, your shoes have been created!"
+      redirect_to outfit_path(@outfit)
     else
+     flash[:alert] = "Ooops, something went wrong!"
       render :new
     end
   end
 
   def destroy
-    @shoe = Shoe.find(params[:id])
+    @outfit = @shoe.outfit
     @shoe.destroy
-    redirect_to outfits_path
+    redirect_to outfit_path(@outfit)
   end
 
   def edit
-    @shoe = Shoe.find(params[:id])
   end
 
   def update
-    @shoe = Shoe.find(params[:id])
     @shoe.update(shoe_params)
-    redirect_to outfits_path
+    redirect_to outfit_path(@outfit)
   end
 
   private
 
   def set_outfit
     @outfit = Outfit.find(params[:outfit_id])
+  end
+
+  def set_shoe
+    @shoe = Shoe.find(params[:id])
   end
 
   def shoe_params
--- app/models/outfit.rb
+++ app/models/outfit.rb
@@ -1,5 +1,5 @@
 class Outfit < ApplicationRecord
   belongs_to :user
-  has_many :shoes
+  has_many :shoes, dependent: :destroy
   validates :brand, presence: true
 end
