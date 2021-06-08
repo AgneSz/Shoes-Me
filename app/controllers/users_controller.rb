e87998f53a61ec1ce8299ce5fac40d20eacc34a5
AgnieszkaSz <piroskavshofeherke@googlemail.com>
2021-06-08 11:04:44 +0100

    User view page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,13 @@
 class UsersController < ApplicationController
   #no methods for user => function supported by devise
   #we will not edit/update/destroy users
+
+  #links for user to: add outfit - could be simple_form or method new from outfit
+  #see all outfits - current_user.outfits
+  #redirect_to => outfit page to dispaly uploaded outfit
+  #simple_form on user page +> takes you to outfit show page
+def show
  @user = current_user
+end
+
 end
