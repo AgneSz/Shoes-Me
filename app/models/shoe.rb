54c05c49726b838341e213e5208285162c7e7e93
Radha Patel <radha13.patel@gmail.com>
2021-06-09 17:54:14 +0100

    show page works with all methods but looks shit

--- app/models/shoe.rb
+++ app/models/shoe.rb
@@ -2,7 +2,7 @@ class Shoe < ApplicationRecord
   belongs_to :outfit
   has_many :feedbacks
   has_many :shoe_categories, dependent: :destroy
-  has_many_attached :photos
+  has_one_attached :photo
   has_many :categories, through: :shoe_categories
   accepts_nested_attributes_for :categories, allow_destroy: true
 
