4c15c359a77d41d9828650aba1d165ddfe3f4caf
Chloe Balanche <chloebalanche@gmail.com>
2021-06-16 12:54:25 +0100

    timeform

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -1,7 +1,7 @@
 import flatpickr from "flatpickr";
-
+import 'flatpickr/dist/flatpickr.css';
 const initFlatpickr = () => {
-  flatpickr(".flattpicker", {enableTime: true});
+  flatpickr(".flattpicker", {enableTime: true, minuteIncrement: 15 });
 }
 
 export { initFlatpickr };
